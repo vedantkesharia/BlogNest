4a3dd4a31d8e1267cb79a4b5058ef463a95dd7a4
vedantkesharia <keshariavedant@gmail.com>
2023-07-05 16:06:14 +0530

    commit12

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ dotenv.config();
 
 const salt = bcrypt.genSaltSync(10);
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'https://theblognest.netlify.app' }));
 app.use(express.json());
 app.use(cookieParser());
 // app.use('/uploads', express.static(__dirname + '/uploads'));
